add131417bfd295ab2196c5246edcc46ed984c1c
hwoy <bosskillerz@gmail.com>
2014-06-24 07:57:11 +0700

    C4 testing:Separate fnum.s from fbasic.s

--- Makefile
+++ Makefile
@@ -1,27 +1,32 @@
 as = as
 ld = ld
-ASFLAGS = --32 -march=i486
+ASFLAGS = --32 -march=i586
 LDFLAGS = -m elf_i386
+libname = libcpuid.a
 bin1 = cpuid.com
 bin2 = saf.com
 
 .PHONY: all clean
 all: $(bin1) $(bin2)
 
-$(bin1): main1.o fbasic.o data.o
-	$(ld) $(LDFLAGS) -N -e _start -Ttext 0x100 --oformat binary -o $(bin1) -M=m1.map main1.o fbasic.o data.o
-$(bin2): main2.o fbasic.o data.o
-	$(ld) $(LDFLAGS) -N -e _start -Ttext 0x100 --oformat binary -o $(bin2) -M=m2.map main2.o fbasic.o data.o
+$(bin1): main1.o $(libname)
+	$(ld) $(LDFLAGS) -N -e _start -Ttext 0x100 --oformat binary -o $(bin1) -M=m1.map main1.o $(libname)
+$(bin2): main2.o $(libname)
+	$(ld) $(LDFLAGS) -N -e _start -Ttext 0x100 --oformat binary -o $(bin2) -M=m2.map main2.o $(libname)
 main1.o: main1.s
 	$(as) $(ASFLAGS) -o main1.o main1.s
 main2.o: main2.s
 	$(as) $(ASFLAGS) -o main2.o main2.s
+$(libname): fbasic.o fnum.o data.o
+	ar -rc $(libname) fbasic.o fnum.o data.o
 fbasic.o: fbasic.s
 	$(as) $(ASFLAGS) -o fbasic.o fbasic.s
+fnum.o: fnum.s
+	$(as) $(ASFLAGS) -o fnum.o fnum.s
 data.o: data.s
 	$(as) $(ASFLAGS) -o data.o data.s
 
 clean:
-	rm -f $(bin1) $(bin2) *.o *~ *.map
+	rm -f $(bin1) $(bin2) *.o *~ *.map *.a
 
 
--- fbasic.s
+++ fbasic.s
@@ -94,110 +94,7 @@ popw %si
 leave
 ret
 
-//*******************************************************    void parsenum(unisigned int num ,unsigned int base,char * buff,const char *parse)
-.globl _parsenum
-_parsenum:
-enter $0,$0
-pushw %si
-pushw %di
 
-xorw %cx,%cx
-movw 4(%bp),%ax
-
-_parsenum_loop:
-xorw %dx,%dx
-
-divw 6(%bp)
-
-pushw %dx
-incw %cx
-testw %ax,%ax
-jz _parsenum_addbuff
-jmp _parsenum_loop
-
-_parsenum_addbuff:
-movw 8(%bp),%di
-
-_parsenum_loop2:
-movw 10(%bp),%si
-testw %cx,%cx
-jz _parsenum_end
-popw %ax
-addw %ax,%si
-movb (%si),%al
-movb %al,(%di)
-incw %di
-decw %cx
-jmp _parsenum_loop2
-
-_parsenum_end:
-movb $0,(%di)
-popw %di
-popw %si
-leave
-ret
-//*******************************************************    void putnum(unsigned int num)
-.globl _putnum
-_putnum:
-enter $17,$0
-leaw -17(%bp),%ax
-
-pushw $_parsestr
-pushw %ax
-pushw $10
-pushw 4(%bp)
-call _parsenum
-addw $8,%sp
-
-leaw -17(%bp),%ax
-pushw %ax
-call _putstr
-popw %cx
-
-leave
-ret
-
-//*******************************************************    void putnumhex(unsigned int num)
-.globl _putnumhex
-_putnumhex:
-enter $17,$0
-leaw -17(%bp),%ax
-
-pushw $_parsestr
-pushw %ax
-pushw $16
-pushw 4(%bp)
-call _parsenum
-addw $8,%sp
-
-leaw -17(%bp),%ax
-pushw %ax
-call _putstr
-popw %cx
-
-leave
-ret
-
-//*******************************************************    void putnumbin(unsigned int num)
-.globl _putnumbin
-_putnumbin:
-enter $17,$0
-leaw -17(%bp),%ax
-
-pushw $_parsestr
-pushw %ax
-pushw $2
-pushw 4(%bp)
-call _parsenum
-addw $8,%sp
-
-leaw -17(%bp),%ax
-pushw %ax
-call _putstr
-popw %cx
-
-leave
-ret
 
 //*******************************************************    void putnl(void)
 .globl _putnl
@@ -355,9 +252,5 @@ leave
 ret
 
 
-
-
-.data
-_parsestr: .asciz "0123456789ABCDEF"
 
 
